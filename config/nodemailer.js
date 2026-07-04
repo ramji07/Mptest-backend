45f66bf5a7d35abc94fb377d1d009ad20269197a
RamjeeC <112837025+RamjeeC@users.noreply.github.com>
2026-07-04 16:36:30 +0530

    bravo

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,21 +4,22 @@ require("dotenv").config();
 const transporter = nodemailer.createTransport({
   host: process.env.BREVO_HOST,
   port: Number(process.env.BREVO_PORT),
-  secure: false,
+  secure: process.env.BREVO_SECURE === "true", // true for 465, false for other ports
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
   },
 });
 
+// console.log("SMTP mail server is Ready:" , transporter.logger);
+
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP Verify Error:", error);
+  } else {
+    console.log("SMTP mail server is Ready:", success);
+  }
+});
+
 module.exports = transporter;
-
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.error("SMTP Verify Error:", error);
-//   } else {
-//     console.log("SMTP mail server is Ready:", success);
-//   }
-// });
-
-// module.exports = transporter;
